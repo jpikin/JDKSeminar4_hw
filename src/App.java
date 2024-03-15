0ce2e2f7c3fb964623afa7aba63440cf675511bd
Pikin <pikin@bk.ru>
2024-03-15 17:17:50 +0500

    add methods added

--- src/App.java
+++ src/App.java
@@ -6,16 +6,28 @@ public class App {
     public static void main(String[] args) {
         EmployeeDirectory employeeList = new EmployeeDirectory();
 
+
         employeeList.addEmployee(new Employee(79222035302L, "Boris", LocalDate.of(2000, 5, 15)));
         employeeList.addEmployee(new Employee(79222035303L, "Evgeniy", LocalDate.of(2008, 3, 11)));
         employeeList.addEmployee(new Employee(79222035304L, "Elena", LocalDate.of(2015, 11, 29)));
         employeeList.addEmployee(new Employee(79222035305L, "Roman", LocalDate.of(2022, 1, 28)));
+
+        employeeList.addEmployeeToHashMap(new Employee("Ivan", 7900433232L, LocalDate.of(2003,2,12)));
+        employeeList.addEmployeeToHashMap(new Employee("Sergey", 7900433321L, LocalDate.of(2012,3,11)));
+        employeeList.addEmployeeToHashMap(new Employee("Olga", 79004234323L, LocalDate.of(2023,1,28)));
+        employeeList.addEmployeeToHashMap(new Employee("Tatiana", 7900443789L, LocalDate.of(2018,7,9)));
+
 
         employeeList.getEmployeeByExperience(5);
 
         employeeList.getEmployeeByExperienceWithFilter(2);
 
         employeeList.getPhoneByName("Boris");
+
+        employeeList.getEmployeeByIdFromList(2);
+
+        employeeList.getEmployeeByIdFromHashMap(5);
+
 
 
     }
--- src/EmployeeDirectory.java
+++ src/EmployeeDirectory.java
@@ -4,21 +4,21 @@ import java.util.List;
 
 public class EmployeeDirectory {
 
-
     private List<Employee> employeeList = new ArrayList<>();
-
     private HashMap<Integer, Employee> employeeHashMap = new HashMap<>();
 
     public void addEmployee(Employee e) {
         this.employeeList.add(e);
     }
-    public void addEmployeeToHashMap(Employee employee){
+
+    public void addEmployeeToHashMap(Employee employee) {
         this.employeeHashMap.put(employee.setID(), employee);
     }
 
     /**
      * Метод получает год и выводит в консоль всех сотрудников,
      * чей стаж равен или меньше запрашиваемому году.
+     *
      * @param year Год, по которому происходит поиск
      */
     public void getEmployeeByExperience(int year) {
@@ -34,29 +34,40 @@ public class EmployeeDirectory {
 
     /**
      * Метод выводит в консоль сотрудников, с совпадающим стажем работы.
+     *
      * @param year год, с которым сравнивается стаж работы сотрудника.
      */
-    public void getEmployeeByExperienceWithFilter(int year){
+    public void getEmployeeByExperienceWithFilter(int year) {
         List<Employee> returnList = employeeList.stream().filter(employee -> employee.getWorkExperience() == year).toList();
-        for (Employee e : returnList){
+        for (Employee e : returnList) {
             System.out.println(e);
         }
-
-
     }
 
     /**
      * Метод получает имя и выводит в консоль имя и телефон сотрудника.
+     *
      * @param name Имя сотрудника
      */
     public void getPhoneByName(String name) {
         boolean check = false;
         for (Employee e : this.employeeList) {
-            if (e.getname().contains(name) ) {
+            if (e.getname().contains(name)) {
                 check = true;
                 System.out.printf("Сотрудник %s: %d", e.getname(), e.getPhoneNumber());
             }
         }
         if (!check) System.out.printf("Сотрудников с именем %s не найдено", name);
+    }
+
+    public void getEmployeeByIdFromList(int id) {
+        List<Employee> returnList = this.employeeList.stream().filter(employee -> employee.getId() == id).toList();
+        for (Employee e : returnList) {
+            System.out.println(e);
+        }
+    }
+
+    public void getEmployeeByIdFromHashMap(int id) {
+        System.out.println((this.employeeHashMap.get(id) != null) ? this.employeeHashMap.get(id) : "С таким ID сотрудников не найдено");
     }
 }
