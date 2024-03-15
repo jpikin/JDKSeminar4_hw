8c93b2c8cbe3ebc6128ac179c32205f10bc132ce
Pikin <pikin@bk.ru>
2024-03-15 17:20:18 +0500

    add some fix

--- src/EmployeeDirectory.java
+++ src/EmployeeDirectory.java
@@ -38,7 +38,8 @@ public class EmployeeDirectory {
      * @param year год, с которым сравнивается стаж работы сотрудника.
      */
     public void getEmployeeByExperienceWithFilter(int year) {
-        List<Employee> returnList = employeeList.stream().filter(employee -> employee.getWorkExperience() == year).toList();
+        List<Employee> returnList = employeeList.stream()
+                .filter(employee -> employee.getWorkExperience() == year).toList();
         for (Employee e : returnList) {
             System.out.println(e);
         }
@@ -61,13 +62,16 @@ public class EmployeeDirectory {
     }
 
     public void getEmployeeByIdFromList(int id) {
-        List<Employee> returnList = this.employeeList.stream().filter(employee -> employee.getId() == id).toList();
+        List<Employee> returnList = this.employeeList.stream()
+                .filter(employee -> employee.getId() == id).toList();
         for (Employee e : returnList) {
             System.out.println(e);
         }
     }
 
     public void getEmployeeByIdFromHashMap(int id) {
-        System.out.println((this.employeeHashMap.get(id) != null) ? this.employeeHashMap.get(id) : "С таким ID сотрудников не найдено");
+        System.out.println((this.employeeHashMap.get(id) != null)
+                ? this.employeeHashMap.get(id)
+                : "С таким ID сотрудников не найдено");
     }
 }
